7fcde54b0f19b6ed131d5460994c3421aa77e1a8
Wilson Franca <wilson.franca.92@gmail.com>
2024-07-10 18:16:36 -0700

    refactor(travelAgent): create a function for research agent and move the code to be reusable

--- travelAgent.py
+++ travelAgent.py
@@ -9,22 +9,19 @@ load_dotenv()
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=os.environ.get("OPENAI_API_KEY"))
 
-tools = load_tools(['ddg-search', 'wikipedia'], llm= llm)
-
-prompt = hub.pull("hwchase17/react")
-
-agent = create_react_agent(
-  llm,
-  tools,
-  prompt,
-)
-
-agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose= True)
-
 query = """"
 I will travel to Lima and Cuzco (Peru) mid May 2025 for 7 days.
 I want you to create a travel itinerary for me with historical sites, must see viewpoints, and cultural events that will be happening during the trip.
 Include the price for flight tickets from Redding, CA, United States to Lima, Peru.
 """
 
-agent_executor.invoke({"input": query})
+def researchAgent(query, llm):
+  tools = load_tools(['ddg-search', 'wikipedia'], llm= llm)
+  prompt = hub.pull("hwchase17/react")
+  agent = create_react_agent(llm, tools, prompt)
+  agent_executor = AgentExecutor(agent=agent, tools=tools, prompt=prompt, verbose= True)
+  webContext = agent_executor.invoke({"input": query})
+  return webContext['output']
+
+
+print(researchAgent(query, llm))
