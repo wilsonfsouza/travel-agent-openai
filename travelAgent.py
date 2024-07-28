de0f42aacf45dd58f720df44a9863f321a823a21
Wilson Franca <wilson.franca.92@gmail.com>
2024-07-28 11:14:06 -0700

    chore: remove commented query

--- travelAgent.py
+++ travelAgent.py
@@ -17,12 +17,6 @@ load_dotenv()
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=os.environ.get("OPENAI_API_KEY"))
 
-# query = """"
-# I will travel to Lima and Cuzco (Peru) mid May 2025 for 7 days.
-# I want you to create a travel itinerary for me with historical sites, must see viewpoints, and cultural events that will be happening during the trip.
-# Include the price for flight tickets from Redding, CA, United States to Lima, Peru.
-# """
-
 def researchAgent(query, llm):
   tools = load_tools(['ddg-search', 'wikipedia'], llm= llm)
   prompt = hub.pull("hwchase17/react")
